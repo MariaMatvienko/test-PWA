08eee068da54e72b39348a0c77b16dcb41e7cb77
Maria Matvienko <meshtli4444@gmail.com>
2022-08-09 10:43:23 +0300

    Updates

--- sw.js
+++ sw.js
@@ -14,9 +14,9 @@ self.addEventListener("install", (e) => {
   );
 });
 
-self.addEventListener("fetch", (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request))
-  );
-});
+// self.addEventListener("fetch", (e) => {
+//   console.log(e.request.url);
+//   e.respondWith(
+//     caches.match(e.request).then((response) => response || fetch(e.request))
+//   );
+// });
