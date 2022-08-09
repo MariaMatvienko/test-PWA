693d7cae653d20eb2331f4729753e810c91df6c1
Maria Matvienko <meshtli4444@gmail.com>
2022-08-09 10:53:32 +0300

    Updates

--- sw.js
+++ sw.js
@@ -1,22 +1,10 @@
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches
-      .open("fox-store")
-      .then((cache) =>
-        cache.addAll([
-          "/my-app/public/",
-          "/my-app/public/index.html",
-          "/my-app/src/",
-          "/my-app/src/index.js",
-          "/my-app/src/style.css",
-        ])
-      )
-  );
+  e.waitUntil(caches.open("fox-store"));
 });
 
-// self.addEventListener("fetch", (e) => {
-//   console.log(e.request.url);
-//   e.respondWith(
-//     caches.match(e.request).then((response) => response || fetch(e.request))
-//   );
-// });
+self.addEventListener("fetch", (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
